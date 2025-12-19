25c167fe4be2dd3fc3c6ee46845ed303b6d46bbc
Ghala <ghalanmasi@icloud.com>
2025-12-19 22:08:04 +0300

    update traveler models and add model location

--- Ma3an/Ma3an/settings.py
+++ Ma3an/Ma3an/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +127,14 @@ STATIC_URL = 'static/'
 
 STRIPE_SECRET_KEY = "sk_test_xxxxxxxxxxxxx"
 STRIPE_PUBLISHABLE_KEY = "pk_test_xxxxxxxxxxxxx"
+
+
+
+MEDIA_URL="/media/"
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+
+
+# Authentication
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- Ma3an/traveler/views.py
+++ Ma3an/traveler/views.py
@@ -1,35 +1,49 @@
+import stripe
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, get_object_or_404, redirect
+from django.conf import settings
+from django.urls import reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Traveler, TourJoin
+from accounts.models import Traveler
+from traveler.models import TravelerPayment
 import random
-# from tours.models import Tour
+from agency.models import Tour, TourSchedule
 
-def traveler_dashboard(request):
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+def traveler_dashboard_view(request):
     traveler = Traveler.objects.get(user=request.user)
-    active_tours = TourJoin.objects.filter(traveler=traveler, status="paid")
+    active_tours = TravelerPayment.objects.filter(traveler=traveler, status="paid")
+
+    # announcements = Announcement.objects.filter(tour__in=[t.tour for t in active_tours])
+    next_event = TourSchedule.objects.filter(
+        tour__in=[t.tour for t in active_tours]
+    ).order_by("event_date").first()
 
     return render(request, "traveler/traveler_dashboard.html", {
         "active_tours": active_tours,
+        # "announcements": announcements,
+        "next_event": next_event,
     })
 
 
-def all_tours(request):
+def tours_view(request):
     query = request.GET.get("q", "")
     tours = Tour.objects.filter(
         Q(tour_name__icontains=query) |
         Q(travel_agency__agencyName__icontains=query)
     )
-    return render(request, "traveler/all_tours.html", {"tours": tours})
+    return render(request, "traveler/tours_view.html", {"tours": tours})
 
-def tour_detail(request, tour_id):
+def tour_detail_view(request, tour_id):
     tour = get_object_or_404(Tour, id=tour_id)
     traveler = Traveler.objects.get(user=request.user)
 
-    joined = TourJoin.objects.filter(traveler=traveler, tour=tour, status="paid").exists()
+    joined = TravelerPayment.objects.filter(traveler=traveler, tour=tour, status="paid").exists()
 
     if request.method == "POST" and not joined:
-        TourJoin.objects.create(
+        TravelerPayment.objects.create(
             traveler=traveler,
             tour=tour,
             amount=tour.price
@@ -42,30 +56,31 @@ def tour_detail(request, tour_id):
     })
 
 
-def payment_view(request, tour_id):
+def traveler_payment_view(request, tour_id):
     traveler = Traveler.objects.get(user=request.user)
-    join = get_object_or_404(TourJoin, traveler=traveler, tour_id=tour_id)
+    join = get_object_or_404(TravelerPayment, traveler=traveler, tour_id=tour_id)
 
-    if request.method == "POST":
-        success = random.choice([True, False])  # simulation
+    intent = stripe.PaymentIntent.create(
+        amount=int(join.amount * 100),  # cents
+        currency="usd",
+        metadata={"tour_id": tour_id, "traveler": traveler.id},
+    )
 
-        if success:
-            join.status = "paid"
-            join.save()
-            messages.success(request, "Payment successful 🎉")
-            return redirect("traveler:tour_detail", tour_id=tour_id)
-        else:
-            join.status = "failed"
-            join.save()
-            messages.error(request, "Payment failed ❌ Please try again.")
-
-    return render(request, "traveler/payment.html", {"join": join})
-
-
-def traveler_dashboard(request):
-    traveler = Traveler.objects.get(user=request.user)
-    active_tours = TourJoin.objects.filter(traveler=traveler, status="paid")
-
-    return render(request, "traveler/traveler_dashboard.html", {
-        "active_tours": active_tours,
+    return render(request, "traveler/payment.html", {
+        "client_secret": intent.client_secret,
+        "stripe_key": settings.STRIPE_PUBLISHABLE_KEY,
+        "join": join
     })
+
+def payment_success(request, tour_id):
+    join = get_object_or_404(TravelerPayment, tour_id=tour_id, traveler__user=request.user)
+    join.status = "paid"
+    join.save()
+    messages.success(request, "Payment successful 🎉")
+    return redirect("traveler:tour_detail_view", tour_id=tour_id)
+
+
+def traveler_required(view_func):
+    return user_passes_test(
+        lambda u: hasattr(u, "traveler")
+    )(view_func)
