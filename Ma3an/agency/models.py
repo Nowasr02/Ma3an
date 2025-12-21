ea47a9c37587d2417a80e0addb3cf54947fa2203
Sana <sanaalboshi77@gmail.com>
2025-12-21 12:09:39 +0300

    pyment model

--- Ma3an/agency/models.py
+++ Ma3an/agency/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 from accounts.models import TourGuide
-from django.db import models
-
+from agency.models import Agency, Subscription
 
 
 # class Agency(models.Model):
@@ -129,3 +127,20 @@ class Subscription(models.Model):
   
     def __str__(self):
         return f"{self.subscriptionType.capitalize()} - ${self.price}"
+    
+
+
+class AgencyPayment(models.Model):
+    agency = models.ForeignKey(Agency, on_delete=models.CASCADE, related_name="payments")
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("paid", "Paid"),
+        ("failed", "Failed"),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.agency} - {self.subscription} - {self.status}"
